e026f5be46dea38a0ed614846b0b309e7ce7f496
Sourov <sourovchandra65@gmail.com>
2025-11-18 18:53:03 +0600

    added new widgth

--- lib/feature/Bottom_items/view/progress_view.dart
+++ lib/feature/Bottom_items/view/progress_view.dart
@@ -4,6 +4,7 @@ import 'package:task_manager_project/common/utils/widget/appbar_widgth.dart';
 import 'package:task_manager_project/feature/Bottom_items/controller/progress_controller.dart';
 
 
+
 class ProgressView extends GetView<ProgressController> {
   const ProgressView({super.key});
 
